8f0c574f04580311f8affd3c0b68e29f14bfa242
dao <77639369+cyangnouvong@users.noreply.github.com>
2026-03-25 23:17:34 -0400

    box class set up

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,21 @@
 import './style.css';
 import * as THREE from 'three';
+import { Box } from './components/Box';
 
 // SETUP
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xfdfbd4);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,
-});
-const cube = new THREE.Mesh(geometry, material);
-cube.rotation.x = 0.5;
-cube.rotation.y = 0.5;
-scene.add(cube);
+const aspect = window.innerWidth / window.innerHeight;
+const frustum = 10;
 
-const camera = new THREE.PerspectiveCamera(
-  75, // field of view in degrees
-  window.innerWidth / window.innerHeight, // aspect ratio
-  0.1, // near clipping plane
-  1000 // far clipping plane
+const camera = new THREE.OrthographicCamera(
+  (frustum * aspect) / -2, // left boundary
+  (frustum * aspect) / 2, // right boundary
+  frustum / 2, // top boundary
+  frustum / -2, // bottom boundary
+  0.1, // near clip
+  1000 // far clip
 );
 camera.position.z = 5;
 
@@ -26,14 +23,15 @@ const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// ANIMATION LOOP
+// OBJECTS
+const box = new Box(scene, 0x545333, { x: 0, y: 0, z: 0 });
+const box2 = new Box(scene, 0x878672, { x: 2, y: 0, z: 5 });
+
+// ANIMATION
 function animate(): void {
   requestAnimationFrame(animate); // Schedule the next frame - infinite loop
-
-  // Rotate the cube for some animation
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+  box.update(); // Update the box's state
+  box2.update(); // Update the second box's state
   // Render the scene from the perspective of the camera
   renderer.render(scene, camera);
 }
@@ -41,7 +39,13 @@ animate();
 
 // RESIZE
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
+  const aspect = window.innerWidth / window.innerHeight;
+
+  camera.left = (frustum * aspect) / -2;
+  camera.right = (frustum * aspect) / 2;
+  camera.top = frustum / 2;
+  camera.bottom = frustum / -2;
   camera.updateProjectionMatrix();
+
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
